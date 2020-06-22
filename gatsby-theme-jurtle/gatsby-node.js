5e53ce2d54ebdbc00c3fc3e859f7c328226a5a2b
yongpilmoon <m71661@gmail.com>
2020-06-23 00:32:08 +0900

    Project init

---
+++ gatsby-theme-jurtle/gatsby-node.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+exports.onPreBootstrap = ({ reporter }) => {
+  const contentPath = 'data'
+  if(!fs.existsSync(contentPath)) {
+    reporter.info(`creating the ${contentPath} directory`)
+    fs.mkdirSync(contentPath)
+  }
+};
+
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type Event implements Node @donInfer {
+      id: ID!
+      name: String!
+      location: String!
+      startDate: Date! @dateformat @proxy(from: "start_date")
+      endDate: Date! @dateformat @proxy(from: "end_date")
+      url: String!
+      slug: String!
+    }
+  `)
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = '/'
+
+  const slugify = str => {
+    const slug = str.toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/(^-|-$)+/g, "")
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, "/")
+  }
+
+  createResolvers({
+    Event: {
+      slug: {
+        resolve: source => slugify(source.name)
+      }
+    }
+  })
+};
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const basePath = '/';
+  actions.createPage({
+    path: basePath,
+    component: require.resolve('./src/templates/events.jsx')
+  })
+  const result = await graphql(`
+    query {
+      allEvent(sort: { fields: startDate, order: ASC }) {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `)
+  if(result.errors) {
+    reporter.panic('error loading events', result.errors)
+    return
+  }
+  const events = result.data.allEvent.nodes
+  events.forEach(event => {
+    const slug = event.slug
+    actions.createPage({
+      path: slug,
+      component: require.resolve('./src/templates/event.js'),
+      context: {
+        eventId: event.id
+      }
+    })
+  })
+};
